f4a1a47b764e508c6a0a844098d4965913f0b966
tt-hasegawa <tt-hasegawa@matsusaka.co.jp>
2020-06-30 10:02:31 +0900

    update requirements.txt

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -2,14 +2,8 @@
 
 sudo apt-get update
 
-sudo apt-get install libhdf5-dev
-sudo apt-get install libjasper-dev
-sudo apt-get install libqtgui4-dev
-sudo apt-get install libqtgui4
-sudo apt-get install libqt4-test
-sudo apt-get install libcblas-dev
-sudo apt-get install libatlas-base-dev
+sudo apt-get install libhdf5-dev libjasper-dev libqtgui4-dev libqtgui4 libqt4-test libcblas-dev libatlas-base-dev
 
 curl https://pjreddie.com/media/files/yolov3.weights -O yolov3.weights
-python convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
+python3 convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
 
